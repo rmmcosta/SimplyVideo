5012b25199cff202a25b55af61fac99f2d72c8f4
rmmcosta <ricardocosta101085@gmail.com>
2021-05-27 08:53:40 +0100

    started adding audio controls

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,8 +13,7 @@ navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
-    addVideoStream('my-video-grid', stream, true, 'myvideoid'); // Display our video to ourselves
-    addMuteUnmuteOption('my-video-grid', 'myvideoid');
+    addVideoStream('my-video-grid', stream, true, 'myvideoid'); // Display our video to ourselves but without outputting sound
     myStream = stream;
 }).catch(err => {
     console.log('Failed to get local stream', err);
@@ -37,9 +36,11 @@ peer.on('call', call => {
         video: true,
         audio: true
     }).then(stream => {
+        console.log('answer call from the user:', call.peer);
         call.answer(stream); // Answer the call with an A/V stream.
         call.on('stream', remoteStream => {
-            addVideoStream('other-video-grid', remoteStream, false);
+            addVideoStream('other-video-grid', remoteStream, false, call.peer);
+            addCustomControls('other-video-grid', call.peer, false);
         });
     }).catch(err => {
         console.log('Failed to get local stream', err);
@@ -54,6 +55,7 @@ socket.on('user-connected', userId => { // If a new user connect
             // Show stream in some video/canvas element
             console.log('remote stream');
             addVideoStream('other-video-grid', remoteStream, false, userId);
+            addCustomControls('other-video-grid', call.peer, false);
         });
     }
 });
@@ -65,25 +67,59 @@ addVideoStream = (parentId, stream, muted, videoId) => {
     videoElement.srcObject = stream;
     videoElement.autoplay = true;
     videoElement.muted = muted;
+    videoElement.volume = 0.5;
     videoElement.id = videoId;
     videoParent.appendChild(videoElement);
 };
 
-addMuteUnmuteOption = (parentId, videoId) => {
-    const muteElement = document.createElement('i');
-    muteElement.className = "fas fa-microphone-slash";
-    muteElement.id = "muteUnmute";
-    muteElement.onclick = () => {
+addCustomControls = (parentId, videoId, isMuted) => {
+    const audioElement = document.createElement('i');
+    audioElement.className = isMuted === true ? "fas fa-volume-mute" : "fas fa-volume-mute transparent";
+    audioElement.id = "audioOnOff";
+    audioElement.onclick = () => {
         const videoElement = document.getElementById(videoId);
         if (videoElement.muted === true) {
-            muteElement.className = "fas fa-microphone";
+            audioElement.className = "fas fa-volume-mute transparent";
             videoElement.muted = false;
         } else {
-            muteElement.className = "fas fa-microphone-slash";
+            audioElement.className = "fas fa-volume-mute";
             videoElement.muted = true;
         }
     };
+    const volumeUpElement = document.createElement('i');
+    volumeUpElement.className = "fas fa-volume-up ml-2";
+    volumeUpElement.id = "volumeUp";
+    volumeUpElement.onclick = () => {
+        const videoElement = document.getElementById(videoId);
+        if (videoElement.volume <= 0.9)
+            videoElement.volume = videoElement.volume + 0.1;
+        else
+            videoElement.volume = 1;
+    };
+    const volumeDownElement = document.createElement('i');
+    volumeDownElement.className = "fas fa-volume-down ml-2";
+    volumeDownElement.id = "volumeDown";
+    volumeDownElement.onclick = () => {
+        const videoElement = document.getElementById(videoId);
+        if (videoElement.volume >= 0.1)
+            videoElement.volume = videoElement.volume - 0.1;
+        else
+            videoElement.volume = 0;
+    };
+    const videoElement = document.getElementById(videoId);
+    const volumeLevelElement = document.createElement('span');
+    volumeLevelElement.className = 'ml-2';
+    volumeLevelElement.id = 'volumeLevel';
+    volumeLevelElement.innerText = (videoElement.volume * 100) + '%';
+    videoElement.onvolumechange = event => {
+        const volumeLevelElement = document.getElementById('volumeLevel');
+        volumeLevelElement.innerText = (event.target.volume * 100) + '%';
+    };
+
     const videoParent = document.getElementById(parentId);
-    videoParent.appendChild(muteElement);
+    videoParent.appendChild(audioElement);
+    videoParent.appendChild(volumeUpElement);
+    videoParent.appendChild(volumeDownElement);
+    videoParent.appendChild(volumeLevelElement);
 };
 
